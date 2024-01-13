82973512b5c642ca7233660b5131e7224ad18613
shaikhirfanali <irfansk.dev@gmail.com>
2024-01-13 15:32:06 +0530

    category crud implemented

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,11 +1,112 @@
+using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Controllers;
 
 public class CategoryController : Controller
 {
-    public IActionResult Index()
+    private readonly ApplicationDbContext _db;
+    
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return View(await _db.Categories.ToListAsync());
+    }
+
+    [HttpGet]
+    public IActionResult Create()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Category category)
+    {
+        /*
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+        }
+        */
+        
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(category);
+            await _db.SaveChangesAsync();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = await _db.Categories.FindAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        
+        return View(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(category);
+            await _db.SaveChangesAsync();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View(category);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = await _db.Categories.FindAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var category = await _db.Categories.FindAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        _db.Categories.Remove(category);
+        await _db.SaveChangesAsync();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
     }
 }
